bc78e42a0abc04be6de7deaf29a853e5523f03b3
Mat Dever <mdever@whoi.edu>
2019-02-28 11:42:29 -0500

    Even newer Fig6
    paticle fluxes are normalized by total number of particles to avoid discussion about spectral slope.

--- figures/code/Fig6_particle_export_flux.m
+++ figures/code/Fig6_particle_export_flux.m
@@ -47,8 +47,7 @@ for ii = 1:5
     clearvars filenameup delimiter formatSpec fileID dataArray ans
 end; clear ii path
 
-flux_meso_down(flux_meso_down==0) = 1e-10;
-flux_meso_up(flux_meso_up==0) = 1e-10;
+
 %% Compute export anomaly based on expected export computed from sinking
 % velocity and distance from 100m horizon
 
@@ -59,14 +58,13 @@ for ws = [0.025 0.5 1 5]
         ind = find(t0+zz/ws<=time_meso,1,'first');
         grav_flux_meso(ind,wscounter) = N0./11./0.125;
     end; clear zz ind
-    grav_flux_meso(:,wscounter) = smooth(grav_flux_meso(:,wscounter),8);
-    a =find(grav_flux_meso(:,wscounter)~=0,1,'first');
-    b = find(grav_flux_meso(:,wscounter)~=0,1,'last');
-    grav_flux_meso(a:b,wscounter) = max(grav_flux_meso(a:b,wscounter));
+    %grav_flux_meso(:,wscounter) = smooth(grav_flux_meso(:,wscounter),12);
+    %a =find(grav_flux_meso(:,wscounter)~=0,1,'first');
+    %b = find(grav_flux_meso(:,wscounter)~=0,1,'last');
+    %grav_flux_meso(a:b,wscounter) = max(grav_flux_meso(a:b,wscounter));
     wscounter = wscounter +1;
 end; clear ws wscounter a b
 
-grav_flux_meso(grav_flux_meso==0) = 1e-10;
 
 %% Then do Papa_submeso
 N0 = 1971717;
@@ -140,37 +138,42 @@ cmap = [0    0.4470    0.7410
 fullfigure
 subplot(3,2,1)
 for ii = 1:5
-    plot(time_meso+105,smooth(flux_meso_up(:,ii)./unique(diff(time_meso)),12),'color',cmap(ii,:),'linewidth',1);
+    plot(time_meso+105,flux_meso_up(:,ii)./unique(diff(time_meso))./N0*100,'color',cmap(ii,:),'linewidth',1);
     hold on; grid on
     theh(ii) = line([0 0],[0 0],'color',cmap(ii,:),'linewidth',2);
 end; clear ii
-set(gca,'yscale','log','xticklabel',{},'fontsize',16,'xlim',[min(time_meso)+105 max(time_meso)+105])
-HH = ylabel('\langleparticle flux\rangle [particles/day]','fontsize',18,'Interpreter','tex');
+set(gca,'yscale','linear','xticklabel',{},'fontsize',16,'xlim',[min(time_meso)+105 max(time_meso)+105])
+HH = ylabel('\langleNormalized particle flux\rangle [%/day]','fontsize',18,'Interpreter','tex');
 POS = get(HH,'position');
 POS(2) = 1;
 set(HH,'position',POS); clear POS
-%plot(time_meso,smooth(grav_flux_meso(:,ii),8),'color',cmap(ii,:),'linestyle','--')
-set(gca,'ylim',[10 12e5],'ytick',[10,100,1000,10000,100000]);
-text(60.75+105,5e5,'(a)','fontweight','b','fontsize',18)
-text(87.5+105,5e5,'Upward flux','fontweight','b','fontsize',18,'HorizontalAlignment','right')
-text(180,5e6,'Papa\_summer','HorizontalAlignment','center','FontWeight','b','FontSize',30)
+set(gca,'ylim',[-1 50],'ytick',0:10:50);
+text(60.75+105,45,'(a)','fontweight','b','fontsize',18)
+text(87.5+105,45,'Upward flux','fontweight','b','fontsize',18,'HorizontalAlignment','right')
+text(180,55,'Papa\_summer','HorizontalAlignment','center','FontWeight','b','FontSize',30)
 
 YLIM = get(gca,'ylim');
 POS = get(gca,'position');
 subplot(3,2,3)
 for ii = 1:5
-    plot(time_meso+105,smooth(flux_meso_down(:,ii)./unique(diff(time_meso)),12),'color',cmap(ii,:),'linewidth',2)
+    plot(time_meso+105,smooth(flux_meso_down(:,ii)./unique(diff(time_meso))/N0*100,8),'color',cmap(ii,:),'linewidth',2)
     hold on; grid on; axis ij
-    plot(time_meso+105,grav_flux_meso(:,ii),'color',cmap(ii,:),'linestyle','--','Linewidth',2)
+    blop = smooth(grav_flux_meso(:,ii)./N0*100,8);
+    if ii == 5
+    blop(29:38) = mean(blop(29:30));
+    elseif ii == 4
+    blop(118:204) = blop(118);
+    end
+    plot(time_meso+105,blop,'color',cmap(ii,:),'linestyle','--','Linewidth',2)
 end; clear ii
 H1 = line([0 0],[0 0],'color','k','linewidth',1);
 H2 = line([0 0],[0 0],'color','k','linewidth',2);
 H3 = line([0 0],[0 0],'color','k','linewidth',2,'linestyle','--');
 POS2 = get(gca,'position');
 POS2(2) = POS(2)-.005-POS2(4);
-set(gca,'ylim',[10 12e5],'ytick',[10,100,1000,10000,100000],'YTickLabel',{'','10^2','10^3','10^4','10^5'},'xlim',[min(time_meso)+105 max(time_meso)+105]);
-text(87.5+105,5e5,'Downward flux','fontweight','b','fontsize',16,'HorizontalAlignment','right')
-set(gca,'yscale','log','ylim',[10 12e5],'position',POS2,'fontsize',16)
+set(gca,'ylim',[0 50],'ytick',10:10:50,'xlim',[min(time_meso)+105 max(time_meso)+105]);
+text(87.5+105,45,'Downward flux','fontweight','b','fontsize',16,'HorizontalAlignment','right')
+set(gca,'yscale','linear','ylim',[0 50],'position',POS2,'fontsize',16)
 xlabel('Day of year','fontsize',18)
 
 % LEGENDS
@@ -186,37 +189,33 @@ set(H,'fontsize',18,'EdgeColor','k','position',[0.1862 0.83 0.2224 0.0208]);clea
 % SUBMESOSCALES
 subplot(3,2,2)
 for ii = 1:5
-    plot(time_submeso,smooth(flux_submeso_up(:,ii)./unique(diff(time_submeso)),12),'color',cmap(ii,:),'linewidth',1)
+    plot(time_submeso,smooth(flux_submeso_up(:,ii)./unique(diff(time_submeso))./N0*100,8),'color',cmap(ii,:),'linewidth',1)
     hold on; grid on
 end; clear ii
-set(gca,'yscale','log','xticklabel',{},'fontsize',16,'xlim',[min(time_submeso) max(time_submeso)])
-%HH = ylabel('\langleparticle flux\rangle [particles/day]','fontsize',18,'Interpreter','tex');
-%POS = get(HH,'position');
-%POS(2) = 1;
-%set(HH,'position',POS); clear POS
+set(gca,'yscale','linear','xticklabel',{},'fontsize',16,'xlim',[min(time_submeso) max(time_submeso)])
 POS = get(gca,'Position');
 POS(1) = 0.48;
 set(gca,'position',POS);
-set(gca,'ylim',[10 12e5],'ytick',[10,100,1000,10000,100000],'yticklabel',{});
-text(50.25,5e5,'(b)','fontweight','b','fontsize',18)
-text(79,5e5,'Upward flux','fontweight','b','fontsize',18,'HorizontalAlignment','right')
-text(65,5e6,'Papa\_winter','HorizontalAlignment','center','FontWeight','b','FontSize',30)
+set(gca,'ylim',[0 50],'ytick',0:10:50,'yticklabel',{});
+text(50.25,45,'(b)','fontweight','b','fontsize',18)
+text(79,45,'Upward flux','fontweight','b','fontsize',18,'HorizontalAlignment','right')
+text(65,55,'Papa\_winter','HorizontalAlignment','center','FontWeight','b','FontSize',30)
 
 YLIM = get(gca,'ylim');
 POS = get(gca,'position');
 subplot(3,2,4)
 for ii = 1:5
-    plot(time_submeso,smooth(flux_submeso_down(:,ii)./unique(diff(time_submeso)),12),'color',cmap(ii,:),'linewidth',1)
+    plot(time_submeso,smooth(flux_submeso_down(:,ii)./unique(diff(time_submeso))./N0*100,8),'color',cmap(ii,:),'linewidth',1)
     hold on; grid on; axis ij
     blop = flux_submeso_down(:,ii)-flux_submeso_up(:,ii);
-    plot(time_submeso,smooth(blop./unique(diff(time_submeso)),12),'color',cmap(ii,:),'linewidth',2)
+    plot(time_submeso,smooth(blop./unique(diff(time_submeso))./N0*100,8),'color',cmap(ii,:),'linewidth',2)
 end; clear ii
 POS2 = get(gca,'position');
 POS2(2) = POS(2)-.005-POS2(4);
 POS2(1) = POS(1);
-set(gca,'ylim',[10 12e5],'ytick',[10,100,1000,10000,100000],'YTickLabel',{''},'xlim',[min(time_submeso) max(time_submeso)]);
-text(79,5e5,'Downward flux','fontweight','b','fontsize',16,'HorizontalAlignment','right')
-set(gca,'yscale','log','ylim',[10 12e5],'position',POS2,'fontsize',16)
+set(gca,'ylim',[0 50],'ytick',10:10:50,'YTickLabel',{''},'xlim',[min(time_submeso) max(time_submeso)]);
+text(79,45,'Downward flux','fontweight','b','fontsize',16,'HorizontalAlignment','right')
+set(gca,'yscale','linear','ylim',[0 50],'position',POS2,'fontsize',16)
 xlabel('Day of year','fontsize',18)
 
 set(gcf,'color','w')
